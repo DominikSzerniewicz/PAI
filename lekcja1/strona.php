e8fbbdebb5d336e1647ca30a8fb4d659b7bed88e
DominikSzerniewicz <dominik_szerniewicz@o2.pl>
2022-09-13 10:44:12 +0200

    lekcja1

---
+++ lekcja1/strona.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+<?php
+$mycounter = 1;
+echo "Hello world"."<br>". $mycounter."<br>". "Hello friend";
+?>
+    </body>
+</html>
