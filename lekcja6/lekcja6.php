cd5c0c3d1535d712cee3a434fefc2906a279d56f
DominikSzerniewicz <dominik_szerniewicz@o2.pl>
2022-09-27 10:11:40 +0200

    tablica

---
+++ lekcja6/lekcja6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Strona szefa</title>
+</head>
+<body>
+
+	<?php
+		$j = 0;
+		$tablica = array("Lalka", "Pan Tadeusz", "Wesele", "Dziady");
+		foreach($tablica as $ksiazki){
+			echo $ksiazki . "<br>"; 
+			
+			++$j;
+		}
+		
+	?>
+
+
+</body>
+</body>
